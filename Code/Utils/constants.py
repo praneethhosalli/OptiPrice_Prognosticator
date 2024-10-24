b369177e8a51e71de19a955cc193466f0237d9a0
vishal7894 <chvishal.me13@gmail.com>
2024-10-24 19:31:55 +0530

    intial commit 01

---
+++ Code/Utils/constants.py
@@ -0,0 +1,8 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DATA_DIR = os.path.join(PROJECT_DIR,"Data")
+RAW_DIR = os.path.join(DATA_DIR,"Raw")
+PROCESSED_DIR = os.path.join(DATA_DIR,"Processed")
+RESULTS_DIR = os.path.join(DATA_DIR,"Results")
+              
