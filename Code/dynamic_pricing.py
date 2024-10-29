bd2c0153d043ce26e36ccb36dbb3dc1e09ba9678
Praneeth <praneethhosalli59@gmail.com>
2024-10-29 18:59:15 +0530

    Updated linear regression model

--- Code/dynamic_pricing.py
+++ Code/dynamic_pricing.py
@@ -7,7 +7,9 @@ from sklearn.metrics import mean_squared_error
 file_path = 'G:\\Spring board internship\\OptiPrice_Prognosticator_Infosys_Internship_Oct2024\\Data\\Raw\\dynamic_pricing.csv'
 data = pd.read_csv(file_path)
 
-X = data.select_dtypes(include='number')
+# X = data.select_dtypes(include='number')
+# X = data.drop(columns=['Historical_Cost_of_Ride'])
+X = data.drop(columns=['Historical_Cost_of_Ride']).select_dtypes(include='number')
 Y = data['Historical_Cost_of_Ride']
 
 X_train, X_temp, Y_train, Y_temp = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -43,3 +45,18 @@ try:
     print("CSV file saved successfully at:", output_file_path)
 except Exception as e:
     print("Error saving CSV:", e)
+
+
+
+# import numpy as np
+
+# # Define a custom input with values for each feature
+# custom_input = np.array([[70, 28, 55, 4.85, 160,]])  # Replace these with your actual test values
+
+# # Reshape to ensure it has the right dimensions (1 row, number of features)
+# custom_input = custom_input.reshape(1, -1)
+
+# # Predict using the trained model
+# predicted_cost = model.predict(custom_input)
+# print("Predicted Historical Cost of Ride:", predicted_cost[0])
+
